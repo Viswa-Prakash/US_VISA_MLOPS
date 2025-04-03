ae2a73752635ead7e3c523c1dca8b7de411e2f51
“Viswa <prakashviswa1990@gmail.com>
2025-04-03 12:52:08 +0530

    requirements.txt and setup.py files created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "us_visa",
+    version = '0.0.1',
+    author = 'Viswa Prakash',
+    author_email = 'prakashviswa1990@gmail.com',
+    packages = find_packages()
+)
